85aff07390a653d2f649d5ee774bd0065d25bf23
kennedyC2 <kennedychidi55@gmail.com>
2022-11-22 02:21:57 +0100

    xxx

--- Frontend_html/assets/js/app.js
+++ Frontend_html/assets/js/app.js
@@ -1,15 +1,21 @@
-let items = document.querySelectorAll("#newArr .carousel-item .grid");
+const populate = (elem) => {
+    let items = document.querySelectorAll("#" + elem + " .carousel-item .grid");
+    console.log(items);
 
-items.forEach((el) => {
-    const minPerSlide = 5;
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0];
+    items.forEach((el) => {
+        const minPerSlide = 5;
+        let next = el.nextElementSibling;
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0];
+            }
+            let cloneChild = next.cloneNode(true);
+            el.appendChild(cloneChild.children[0]);
+            next = next.nextElementSibling;
         }
-        let cloneChild = next.cloneNode(true);
-        el.appendChild(cloneChild.children[0]);
-        next = next.nextElementSibling;
-    }
-});
+    });
+};
+
+populate("newArrival");
+populate("trending");
